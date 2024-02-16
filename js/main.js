7ffa9a7e4995a602a1d77d69bbd765ea3110aacf
mycodingco <153215714+mycodingco@users.noreply.github.com>
2024-02-16 11:13:59 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ new Swiper('.awards .swiper-container', {
   autoplay: true,
   loop: true,
   spaceBetween: 30,
-  slidesPerView: 5
+  slidesPerView: 5,
   navigation: {
     prevEl: '.awards .swiper-prev',
     nextEl: '.awards .swiper-next'
